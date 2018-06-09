0482dd20305b27a7e2090dda6595abc31e229c0a
klolik <klolik@ya.ru>
2018-06-09 14:39:35 +0300

    events

---
+++ events.py
@@ -0,0 +1,40 @@
+from interfaces import (
+    EventInterface,
+    EventSerializerComponent,
+    EventPublishOptsComponent
+)
+
+
+class CommonPublishOpts(EventPublishOptsComponent):
+    @staticmethod
+    def ttl():
+        return None
+
+
+class CommonEventSerialize(EventSerializerComponent):
+    def serialize(event: EventInterface) -> dict:
+        return {
+            'type': event.event_type(),
+            'data': event.__dict__,
+        }
+
+    @classmethod
+    def deserialize(cls, serialize_data):
+        if serialize_data.get('type') != cls.event_type():
+            return None
+
+        if not isinstance(serialize_data.get('data'), dict):
+            return None
+
+        ev = cls()
+        ev.__dict__.update(serialize_data['data'])
+        return ev
+
+
+class CommonEvent(EventInterface, CommonEventSerialize, CommonPublishOpts):
+    def __init__(self, message=None):
+        self.message = message
+
+    @staticmethod
+    def event_type():
+        return 'Common event'
